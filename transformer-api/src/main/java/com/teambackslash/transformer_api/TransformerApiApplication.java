d7adf2c99b338cba2b9fd012012764b41a867aa7
Samudra Uduwaka <samudrauduwaka@gmail.com>
2025-07-27 20:30:21 +0530

    Initialize transformer-api with Java Spring Boot

---
+++ transformer-api/src/main/java/com/teambackslash/transformer_api/TransformerApiApplication.java
@@ -0,0 +1,13 @@
+package com.teambackslash.transformer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransformerApiApplication.class, args);
+	}
+
+}
---
+++ transformer-api/src/test/java/com/teambackslash/transformer_api/TransformerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teambackslash.transformer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransformerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
