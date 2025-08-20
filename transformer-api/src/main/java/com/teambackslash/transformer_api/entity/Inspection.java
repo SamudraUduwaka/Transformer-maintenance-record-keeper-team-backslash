6677ea5734f678b8d0d28a567844fce14eb7837f
InduwaraGayashan001 <induwaragayashan@gmail.com>
2025-08-21 00:35:23 +0530

    Add entities

---
+++ transformer-api/src/main/java/com/teambackslash/transformer_api/entity/Inspection.java
@@ -0,0 +1,35 @@
+package com.teambackslash.transformer_api.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "inspection")
+@Data
+public class Inspection {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "inspection_id")
+    private Integer inspectionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "transformer_no", nullable = false)
+    private Transformer transformer;
+
+    @Column(name = "inspection_time", nullable = false)
+    private LocalDateTime inspectionTime;
+
+    @Column(name = "branch", nullable = false, length = 100)
+    private String branch;
+
+    @Column(name = "inspector", nullable = false, length = 100)
+    private String inspector;
+
+    @OneToMany(mappedBy = "inspection", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images;
+
+}
