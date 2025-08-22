6942ccc752b785a2a47a82851761ce895c70cf0d
thisara-g <thisara200306@gmail.com>
2025-08-22 11:19:18 +0530

    New route added

--- transformer-dashboard/src/App.tsx
+++ transformer-dashboard/src/App.tsx
@@ -1,15 +1,20 @@
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Dashboard from "./pages/Dashboard"
-import "./App.css"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import TransformerInspection from "./pages/TransformerInspection"; // 👈 import new page
+import "./App.css";
 
 function App() {
   return (
-  <Router>
+    <Router>
       <Routes>
+        {/* Main dashboard with Transformers + Inspections views */}
         <Route path="/" element={<Dashboard />} />
-      </Routes> 
-  </Router>
-  )
+
+        {/* Transformer-specific inspection detail page */}
+        <Route path="/inspections/:transformerNo" element={<TransformerInspection />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
