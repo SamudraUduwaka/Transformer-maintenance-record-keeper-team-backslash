d7c0ae6471614a4810f6097f8cebb901d2d5ed46
InduwaraGayashan001 <induwaragayashan@gmail.com>
2025-10-03 10:02:33 +0530

    Refactor BoundingBoxDTO to use Lombok annotations

--- transformer-api/src/main/java/com/teambackslash/transformer_api/dto/BoundingBoxDTO.java
+++ transformer-api/src/main/java/com/teambackslash/transformer_api/dto/BoundingBoxDTO.java
@@ -1,62 +1,15 @@
 package com.teambackslash.transformer_api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+@AllArgsConstructor
+@Data
+@ToString
 public class BoundingBoxDTO {
     private Integer x;
     private Integer y;
     private Integer width;
     private Integer height;
 
-    // Default constructor
-    public BoundingBoxDTO() {}
-
-    // Constructor with parameters
-    public BoundingBoxDTO(Integer x, Integer y, Integer width, Integer height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
-
-    // Getters and Setters
-    public Integer getX() {
-        return x;
-    }
-
-    public void setX(Integer x) {
-        this.x = x;
-    }
-
-    public Integer getY() {
-        return y;
-    }
-
-    public void setY(Integer y) {
-        this.y = y;
-    }
-
-    public Integer getWidth() {
-        return width;
-    }
-
-    public void setWidth(Integer width) {
-        this.width = width;
-    }
-
-    public Integer getHeight() {
-        return height;
-    }
-
-    public void setHeight(Integer height) {
-        this.height = height;
-    }
-
-    @Override
-    public String toString() {
-        return "BoundingBoxDTO{" +
-                "x=" + x +
-                ", y=" + y +
-                ", width=" + width +
-                ", height=" + height +
-                '}';
-    }
 }
