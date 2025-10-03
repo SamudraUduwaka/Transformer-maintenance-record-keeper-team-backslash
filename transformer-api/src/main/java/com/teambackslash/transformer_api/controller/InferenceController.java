54c7b35e69e9a0bf9a6661f78a9d8e7a95b41fe9
dilsha01 <mihirangangd.21@uom.lk>
2025-10-03 19:13:48 +0530

    Add InferenceController for handling prediction requests

---
+++ transformer-api/src/main/java/com/teambackslash/transformer_api/controller/InferenceController.java
@@ -0,0 +1,23 @@
+package com.teambackslash.transformer_api.controller;
+
+import com.teambackslash.transformer_api.dto.PredictionDTO;
+import com.teambackslash.transformer_api.service.PythonInferenceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/inference")
+@RequiredArgsConstructor
+public class InferenceController {
+
+    private final PythonInferenceService inferenceService;
+
+    @PostMapping(value = "/predict", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<PredictionDTO> predict(@RequestPart("file") MultipartFile file) {
+        PredictionDTO dto = inferenceService.runInference(file);
+        return ResponseEntity.ok(dto);
+    }
+}
