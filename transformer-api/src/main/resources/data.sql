67dfd5a085712099cbeb073e302937d3dac4ebd6
thisara-g <thisara200306@gmail.com>
2025-10-05 16:55:25 +0530

    Baseline images updated according to given sample of images

--- transformer-api/src/main/resources/data.sql
+++ transformer-api/src/main/resources/data.sql
@@ -40,22 +40,22 @@ VALUES
 INSERT IGNORE INTO image
   (inspection_id, transformer_no, image_url, type, weather_condition, created_at, updated_at)
 VALUES
-(1,'AZ-8801','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756301699/p9094_ahmqc3.bmp','baseline','Sunny', NOW(), NOW()),
-(6,'AZ-8801','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756301659/p6001_udjhob.bmp','baseline','Cloudy', NOW(), NOW()),
-(11,'AZ-8801','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756301580/p3027_nzgwvf.bmp','baseline','Rainy', NOW(), NOW()),
+(1,'AZ-8801','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662094/T1_normal_004_gylsee.jpg','baseline','Sunny', NOW(), NOW()),
+(6,'AZ-8801','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662113/T1_normal_003_olh7pr.jpg','baseline','Cloudy', NOW(), NOW()),
+(11,'AZ-8801','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662053/T1_normal_001_e0b706.jpg','baseline','Rainy', NOW(), NOW()),
 
-(2,'AZ-8802','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756301689/p8053_nglld9.bmp','baseline','Sunny', NOW(), NOW()),
-(7,'AZ-8802','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756301645/p5027_ndtpdt.bmp','baseline','Cloudy', NOW(), NOW()),
-(12,'AZ-8802','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756301516/p1001_hke7at.bmp','baseline','Rainy', NOW(), NOW()),
+(2,'AZ-8802','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662646/T4_normal_001_lxkhqf.png','baseline','Sunny', NOW(), NOW()),
+(7,'AZ-8802','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662662/T5_normal_001_attxhb.png','baseline','Cloudy', NOW(), NOW()),
+(12,'AZ-8802','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662602/T2_normal_001_eydful.png','baseline','Rainy', NOW(), NOW()),
 
-(3,'AZ-8803','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756301672/p7026_knf0ft.bmp','baseline','Sunny', NOW(), NOW()),
-(8,'AZ-8803','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756301627/p4001_cioj3g.bmp','baseline','Cloudy', NOW(), NOW()),
-(13,'AZ-8803','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756302533/p2001_udsaya.bmp','baseline','Rainy', NOW(), NOW()),
+(3,'AZ-8803','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662213/T6_normal_001_lzomfh.jpg','baseline','Sunny', NOW(), NOW()),
+(8,'AZ-8803','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662207/T6_normal_002_cd2s1e.jpg','baseline','Cloudy', NOW(), NOW()),
+(13,'AZ-8803','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662211/T6_normal_003_jrstug.jpg','baseline','Rainy', NOW(), NOW()),
 
-(4,'AZ-8804','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756302684/p9085_u3duca.bmp','baseline','Sunny', NOW(), NOW()),
-(9,'AZ-8804','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756302601/p6001_bgexf3.bmp','baseline','Cloudy', NOW(), NOW()),
-(14,'AZ-8804','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756302569/p4001_bos6as.bmp','baseline','Rainy', NOW(), NOW()),
+(4,'AZ-8804','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662383/T7_normal_002_rclksi.jpg','baseline','Sunny', NOW(), NOW()),
+(9,'AZ-8804','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662391/T7_normal_003_wjcvui.jpg','baseline','Cloudy', NOW(), NOW()),
+(14,'AZ-8804','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662380/T7_normal_001_v4zo5j.jpg','baseline','Rainy', NOW(), NOW()),
 
-(5,'AZ-8805','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756302619/p8053_qm2iwj.bmp','baseline','Sunny', NOW(), NOW()),
-(10,'AZ-8805','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756302579/p5027_amdy3m.bmp','baseline','Cloudy', NOW(), NOW()),
-(15,'AZ-8805','https://res.cloudinary.com/ddleqtgrj/image/upload/v1756302544/p3027_e8kga1.bmp','baseline','Rainy', NOW(), NOW());
+(5,'AZ-8805','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662419/T8_normal_002_huap8c.jpg','baseline','Sunny', NOW(), NOW()),
+(10,'AZ-8805','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662414/T8_normal_003_z1eplm.jpg','baseline','Cloudy', NOW(), NOW()),
+(15,'AZ-8805','https://res.cloudinary.com/ddleqtgrj/image/upload/v1759662425/T8_normal_001_pficpv.jpg','baseline','Rainy', NOW(), NOW());
