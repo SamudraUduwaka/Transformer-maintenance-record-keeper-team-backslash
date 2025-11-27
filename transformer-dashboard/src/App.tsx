3ce16718666044a60dcbb792b638e82ddf9b0b5e
Samudra Uduwaka <samudrauduwaka@gmail.com>
2025-11-27 15:28:37 +0530

    fix: Update routes to include settings page

--- transformer-dashboard/src/App.tsx
+++ transformer-dashboard/src/App.tsx
@@ -7,6 +7,7 @@ import Dashboard from "./pages/Dashboard";
 import TransformerInspection from "./pages/TransformerInspection";
 import InspectionDetails from "./pages/InspectionDetails";
 import DigitalMaintenanceForm from "./pages/DigitalMaintenanceForm";
+import Settings from "./pages/Settings";
 import "./App.css";
 import { AuthProvider } from "./context/AuthContext";
 import Login from "./pages/Login";
@@ -33,6 +34,7 @@ function App() {
                 path="/digital-form/:transformerNo/:inspectionNo"
                 element={<DigitalMaintenanceForm />}
               />
+              <Route path="/settings" element={<Settings />} />
             </Routes>
           </Router>
         </ThemeProvider>
